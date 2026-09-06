263c9863ec0dd815a2cd70545d03445286278ca1
AYUSH <dixitayush6394@gmail.com>
2026-09-06 19:20:33 +0530

    fix:axios instance

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,9 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:3000"
+  : import.meta.env.VITE_API_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
